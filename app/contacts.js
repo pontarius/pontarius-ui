857c420b6d6b72cff2d33262c7bd64074bd7ce4b
Jon Kristensen <info@jonkri.com>
2015-07-26 22:04:33 +0200

    Add app-related work in progress (a contact window)

--- app/contacts.js
+++ app/contacts.js
@@ -20,7 +20,6 @@ with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-const GConf = imports.gi.GConf;
 const Gdk = imports.gi.Gdk;
 const Gio = imports.gi.Gio;
 const GObject = imports.gi.GObject;
